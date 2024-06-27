04637cac1f51fd4318e58924cf961f89027d5c4f
moonsujung26 <mmm021668@gmail.com>
2024-06-27 17:25:39 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,36 @@
 $(function () {
 
+
     $('.ht .inner .site>a').on('click', function (e) {
         e.preventDefault();
         $(this).next().toggle();
     });
+
     $('.ht .inner .lang>a').on('click', function (e) {
         e.preventDefault();
         $(this).next().toggle();
     });
 
 
-    const mainVisualSlide = new Swiper('.main_visual_slide',{
+    const mainVisualSlide = new Swiper('.main_visual_slide', {
+        loop: true,
+        speed:800,
+        // autoplay: {
+        //     delay:2500,
+        //     disableOnInteraction: false,
+        // },
+        pagination: {
+            el: "#main_visual .main_visual_slide .dots",
+          },
+       
+    });
+
+
+
+    const mainOfferSlide = new Swiper('.main_offers_slide', {
+        slidesPerView: 3,
 
     })
-
 
 
 });
