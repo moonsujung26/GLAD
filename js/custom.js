48787060b5dbad6b5dbbdfb620f9052ac93393ea
moonsujung26 <mmm021668@gmail.com>
2024-07-05 15:26:34 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,12 +3,12 @@ $(function () {
 
     $('.ht .inner .site>a').on('click', function (e) {
         e.preventDefault();
-        $(this).next().toggle();
+        $(this).next().slideToggle();
     });
 
     $('.ht .inner .lang>a').on('click', function (e) {
         e.preventDefault();
-        $(this).next().toggle();
+        $(this).next().slideToggle();
     });
 
 
@@ -36,9 +36,79 @@ $(function () {
         },
 
     });
-    const mainFacilitySlide = new Swiper('#main_facility .left_slide', {
-        slidesPerView: 3.5,
+    const mainFacilitySlide = new Swiper('#main_facility .facility_slide', {
+        slidesPerView: 1.8,
+        centeredSlides: true,
+        spaceBetween: 30,
+        loop: true,
+        speed: 800,
+        // effect: "fade",
+        pagination: {
+            el: "#main_facility .dots",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: "#main_facility .arrows .prev",
+            prevEl: "#main_facility .arrows .next",
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
 
-    })
+    });
+
+    const mainDiningSlide01 = new Swiper('#main_dining .tap_con .tap_slide01', {
+        slidesPerView: 1,
+        loop: true,
+        effect: "fade",
+        speed: 800,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+    const mainDiningSlide02 = new Swiper('#main_dining .tap_con .tap_slide02', {
+        slidesPerView: 1,
+        loop: true,
+        speed: 800,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+    const mainDiningSlide03 = new Swiper('#main_dining .tap_con .tap_slide03', {
+        slidesPerView: 1,
+        loop: true,
+        speed: 800,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+
+    $('#main_dining .inner .tap_menu li button').on('click', function () {
+        $(this)
+            .parent()
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+
+        let idx = $(this).parent().index();
+
+        $('#main_dining .tap_con .con')
+            .eq(idx)
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+
+        $(this).next().stop().slideToggle();
+        $(this).parent().stop().siblings().find('.desc').slideUp();
+
+    });
+
+
+
+
 
 });
